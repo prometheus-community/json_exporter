fe22a839e24110560f209e76f4a19bca5694ef67
rustyclock <rustyclock@protonmail.com>
2021-01-24 12:51:34 +0900

    Migrate JSONPath library
    Signed-off-by: rustyclock <rustyclock@protonmail.com>

--- exporter/util.go
+++ exporter/util.go
@@ -26,7 +26,6 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/kawamuray/jsonpath"
 	"github.com/prometheus-community/json_exporter/config"
 	"github.com/prometheus/client_golang/prometheus"
 	pconfig "github.com/prometheus/common/config"
@@ -36,40 +35,30 @@ func MakeMetricName(parts ...string) string {
 	return strings.Join(parts, "_")
 }
 
-func SanitizeValue(v *jsonpath.Result) (float64, error) {
+func SanitizeValue(s string) (float64, error) {
 	var value float64
-	var boolValue bool
-	var err error
-	switch v.Type {
-	case jsonpath.JsonNumber:
-		value, err = parseValue(v.Value)
-	case jsonpath.JsonString:
-		// If it is a string, lets pull off the quotes and attempt to parse it as a number
-		value, err = parseValue(v.Value[1 : len(v.Value)-1])
-	case jsonpath.JsonNull:
-		value = math.NaN()
-	case jsonpath.JsonBool:
-		if boolValue, err = strconv.ParseBool(string(v.Value)); boolValue {
-			value = 1.0
-		} else {
-			value = 0.0
-		}
-	default:
-		value, err = parseValue(v.Value)
-	}
-	if err != nil {
-		// Should never happen.
-		return -1.0, err
-	}
-	return value, err
-}
+	var resultErr string
 
-func parseValue(bytes []byte) (float64, error) {
-	value, err := strconv.ParseFloat(string(bytes), 64)
-	if err != nil {
-		return -1.0, fmt.Errorf("failed to parse value as float; value: %q; err: %w", bytes, err)
+	if value, err := strconv.ParseFloat(s, 64); err == nil {
+		return value, nil
+	} else {
+		resultErr = fmt.Sprintf("%s", err)
 	}
-	return value, nil
+
+	if boolValue, err := strconv.ParseBool(s); err == nil {
+		if boolValue {
+			return 1.0, nil
+		} else {
+			return 0.0, nil
+		}
+	} else {
+		resultErr = resultErr + "; " + fmt.Sprintf("%s", err)
+	}
+
+	if s == "<nil>" {
+		return math.NaN(), nil
+	}
+	return value, fmt.Errorf(resultErr)
 }
 
 func CreateMetricsList(c config.Config) ([]JsonMetric, error) {
@@ -77,23 +66,17 @@ func CreateMetricsList(c config.Config) ([]JsonMetric, error) {
 	for _, metric := range c.Metrics {
 		switch metric.Type {
 		case config.ValueScrape:
-			constLabels := make(map[string]string)
 			var variableLabels, variableLabelsValues []string
 			for k, v := range metric.Labels {
-				if len(v) < 1 || v[0] != '$' {
-					// Static value
-					constLabels[k] = v
-				} else {
-					variableLabels = append(variableLabels, k)
-					variableLabelsValues = append(variableLabelsValues, v)
-				}
+				variableLabels = append(variableLabels, k)
+				variableLabelsValues = append(variableLabelsValues, v)
 			}
 			jsonMetric := JsonMetric{
 				Desc: prometheus.NewDesc(
 					metric.Name,
 					metric.Help,
 					variableLabels,
-					constLabels,
+					nil,
 				),
 				KeyJsonPath:     metric.Path,
 				LabelsJsonPaths: variableLabelsValues,
@@ -102,23 +85,17 @@ func CreateMetricsList(c config.Config) ([]JsonMetric, error) {
 		case config.ObjectScrape:
 			for subName, valuePath := range metric.Values {
 				name := MakeMetricName(metric.Name, subName)
-				constLabels := make(map[string]string)
 				var variableLabels, variableLabelsValues []string
 				for k, v := range metric.Labels {
-					if len(v) < 1 || v[0] != '$' {
-						// Static value
-						constLabels[k] = v
-					} else {
-						variableLabels = append(variableLabels, k)
-						variableLabelsValues = append(variableLabelsValues, v)
-					}
+					variableLabels = append(variableLabels, k)
+					variableLabelsValues = append(variableLabelsValues, v)
 				}
 				jsonMetric := JsonMetric{
 					Desc: prometheus.NewDesc(
 						name,
 						metric.Help,
 						variableLabels,
-						constLabels,
+						nil,
 					),
 					KeyJsonPath:     metric.Path,
 					ValueJsonPath:   valuePath,
---
+++ exporter/util_test.go
@@ -0,0 +1,59 @@
+// Copyright 2020 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exporter
+
+import (
+	"math"
+	"testing"
+)
+
+func TestSanitizeValue(t *testing.T) {
+	tests := []struct {
+		Input          string
+		ExpectedOutput float64
+		ShouldSucceed  bool
+	}{
+		{"1234", 1234.0, true},
+		{"1234.5", 1234.5, true},
+		{"true", 1.0, true},
+		{"TRUE", 1.0, true},
+		{"False", 0.0, true},
+		{"FALSE", 0.0, true},
+		{"abcd", 0, false},
+		{"{}", 0, false},
+		{"[]", 0, false},
+		{"", 0, false},
+		{"''", 0, false},
+	}
+
+	for i, test := range tests {
+		actualOutput, err := SanitizeValue(test.Input)
+		if err != nil && test.ShouldSucceed {
+			t.Fatalf("Value snitization test %d failed with an unexpected error.\nINPUT:\n%q\nERR:\n%s", i, test.Input, err)
+		}
+		if test.ShouldSucceed && actualOutput != test.ExpectedOutput {
+			t.Fatalf("Value sanitization test %d fails unexpectedly.\nGOT:\n%f\nEXPECTED:\n%f", i, actualOutput, test.ExpectedOutput)
+		}
+	}
+}
+
+func TestSanitizeValueNaN(t *testing.T) {
+	actualOutput, err := SanitizeValue("<nil>")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !math.IsNaN(actualOutput) {
+		t.Fatalf("Value sanitization test for %f fails unexpectedly.", math.NaN())
+	}
+}
